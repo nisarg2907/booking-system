06acfe5a45a585aec3837ad8ce7faba31a0e5b3a
Nisarg <nisargshah.ict19@gmail.com>
2024-02-09 13:30:40 +0530

    only thing remaining is to show faillure snackbar on 40 status from backend

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -5,5 +5,5 @@ const bookingController = require("../controllers/reservation");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/all-rooms",roomController.getAllRooms);
-router.post("/book-room", bookingController.bookRoom);
+router.post("/book-room", authMiddleware.verifyToken, bookingController.bookRoom);
 module.exports = router;
